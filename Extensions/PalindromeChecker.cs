0b8627f89ed5a88115684b8501c9886b5851cf54
Ayobami Adewole <ayobami@users.noreply.github.com>
2018-03-26 08:50:34 +0100

    Extension project

---
+++ Extensions/PalindromeChecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public class PalindromeChecker
+    {
+        public bool IsWordPalindrome(string word)
+        {
+            return word.SequenceEqual(word.Reverse());
+        }
+    }
+}
---
+++ Extensions/PalindromeCheckerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Extensions
+{
+    public class PalindromeCheckerTest
+    {
+        [Theory, SqlServerData(@".\sqlexpress", "TheoryDb", "select word from Palindrome")]
+        public void Test_IsWordPalindrome_ShouldReturnTrue(string word)
+        {
+            PalindromeChecker palindromeChecker = new PalindromeChecker();
+            Assert.True(palindromeChecker.IsWordPalindrome(word));
+        }
+
+
+        //[Theory, CsvData(@"C:\data.txt", false)]
+        //public void Test_IsWordPalindrome_ShouldReturnTrue(string word)
+        //{
+        //    PalindromeChecker palindromeChecker = new PalindromeChecker();
+
+        //    Assert.True(palindromeChecker.IsWordPalindrome(word));
+
+
+        //}
+    }
+}
